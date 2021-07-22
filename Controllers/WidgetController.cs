fcb13148f8dccfd001d6436d49660d4029d42a98
Timothy Fox <timothyfox443@gmail.com>
2021-07-22 16:13:35 -0700

    Add project files.

---
+++ Controllers/WidgetController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVCFormNotBinding.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCFormNotBinding.Controllers
+{
+    public class WidgetController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddNoBind()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNoBind(IFormCollection form)
+        {
+            Widget product = new Widget()
+            {
+                objName = form["product-name"],
+                objDesc = form["product-description"],
+                objWeight = Convert.ToDouble(form["unit-weight"]),
+                objUnitCost = Convert.ToDouble(form["unit-cost"]),
+                objSalePrice = Convert.ToDouble(form["unit-price"])
+            };
+
+            //add code here to insert object into DB that isn't created
+
+            //oh and make the DB
+
+            ViewData["isAddedNoBind"] = $"{product.objName} has been created and added to the, oh wait nevermind.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddWithBind()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddWithBind(Widget product)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["isAddedYesBind"] = $"{product.objName} has been created and added to the, oh wait nevermind.";
+            }
+            return View();
+        }
+
+
+    }
+}
---
+++ Models/Widget.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCFormNotBinding.Models
+{
+    public class Widget
+    {
+        /// <summary>
+        /// Object name
+        /// </summary>
+        [Display(Name = "Product Name")]
+        [Required(ErrorMessage ="A name is required.")]
+        public String objName { get; set; }
+
+        /// <summary>
+        /// Object Description
+        /// </summary>
+        [Display(Name = "Product Description")]
+        [Required(ErrorMessage = "A description is required.")]
+
+        public String objDesc { get; set; }
+
+        /// <summary>
+        /// Object Unit Weight
+        /// </summary>
+        [Display(Name = "Product Unit Weight")]
+        [Required(ErrorMessage = "A weight in decimal format is required.")]
+
+        public Double objWeight { get; set; }
+
+        /// <summary>
+        /// Object Unit Cost to Purchase
+        /// </summary>
+        [Display(Name = "Product Unit Cost")]
+        [Required(ErrorMessage = "A cost in $(USD) is required.")]
+
+        public Double objUnitCost { get; set; }
+
+        /// <summary>
+        /// Object Customer Price for Sales
+        /// </summary>
+        [Display(Name = "Product Customer Price")]
+        [Required(ErrorMessage = "A price in $(USD) is required.")]
+
+        public Double objSalePrice { get; set; }
+    }
+}
